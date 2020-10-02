b95e3af907bdee3d0ee46536fb37e7c662e25f87
qiaoli wang <414217795zoe@gmail.com>
2020-10-02 12:02:17 +1000

    project init

---
+++ controllers/lobbyController.js
@@ -0,0 +1,25 @@
+const DB = require('../services/MongoService')
+
+// logic
+const addTest=(addTest,res)=>{
+
+    // business logic
+
+
+    // call external service db
+}
+
+const updateTest=()=>{
+
+}
+
+const deleteTest=()=>{
+
+}
+
+// export
+module.exports ={
+    addTest,
+    updateTest,
+    deleteTest
+}
