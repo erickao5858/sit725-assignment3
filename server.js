9fbbd381b4f47aa2479c0e9377295aca45edd8a5
sjothishunmugam <sjothishunmugam@deakin.edu.au>
2020-10-18 20:23:03 +1100

    Completed home and rules page

--- server.js
+++ server.js
@@ -1,14 +1,30 @@
 const express = require('express')
 const bodyParser = require('body-parser');
+const path = require('path');
 const app = express()
 const mongo = require('./services/MongoService')
-const PORT = 3000;
+const PORT = 7070;
 
 let http = require('http').createServer(app);
 let io = require('socket.io')(http);
 
+
 // for hosting static files (html)
 app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'html'); 
+app.all('/rules',function(req,res){
+    res.sendFile(path.join(__dirname+'/public/rules.html'));
+    // return res.redirect('/index');
+});
+
+app.all('/lobby',function(req,res){
+    res.sendFile(path.join(__dirname+'/public/lobby.html'));
+    // return res.redirect('/index');
+});
+
+
+
+
 
 // need to add the body parser so that we can extract the body data
 app.use(bodyParser.urlencoded({
@@ -32,6 +48,7 @@ io.on('connection', (socket) => {
 mongo.startDB()
 
 // liston to the port 3000
-http.listen(PORT,function () {
+http.listen(PORT,function (err) {
+    if (err) console.log(err); 
     console.log(`web server running at: http://localhost:${PORT}`)
 })
