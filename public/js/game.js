ecdd848b9dd9cd0080b8749ece8092ed3605d6ef
CHIEN-CHENG KAO <eric.kao5858@gmail.com>
2020-10-20 19:16:59 +1100

    Prepare data for display remaining roles function

--- public/js/game.js
+++ public/js/game.js
@@ -62,3 +62,32 @@ const appendPlayer = () => {
         player.find('.equipment-scope').attr('title', SCOPE).html('<b>Scope</b>')
     }
 }
+
+
+// TODO For Zilin Add UI for remaining roles
+
+// Json data
+
+const players = [{
+        "index": 0,
+        "isDead": false,
+        "role": "Renegade"
+    },
+    {
+        "index": 1,
+        "isDead": true,
+        "role": "Outlaw"
+    },
+    {
+        "index": 2,
+        "isDead": false,
+        "role": "Renegade"
+    },
+    {
+        "index": 3,
+        "isDead": true,
+        "role": "Renegade"
+    }
+]
+
+console.log(players)
